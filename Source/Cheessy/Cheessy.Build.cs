934766c204177c27f1fac2ae154084e2e8e446d6
ArmainAP <adrianmarianpopoviciu@gmail.com>
2017-04-07 20:51:11 +0300

    Version 0.0.4
    Added scalability settings.

--- Source/Cheessy/Cheessy.Build.cs
+++ Source/Cheessy/Cheessy.Build.cs
@@ -6,7 +6,7 @@ public class Cheessy : ModuleRules
 {
 	public Cheessy(TargetInfo Target)
 	{
-		PublicDependencyModuleNames.AddRange(new string[] { "Core", "CoreUObject", "Engine", "InputCore" });
+		PublicDependencyModuleNames.AddRange(new string[] { "Core", "CoreUObject", "Engine", "InputCore", "RHI" });
 
 		PrivateDependencyModuleNames.AddRange(new string[] {  });
 
